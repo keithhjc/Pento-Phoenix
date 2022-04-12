29ca1f471ee03654000c0e0d904fac029a62082e
kvxium <keith.ho@pixiumdigital.com>
2022-04-12 13:36:23 +0800

    :art: setting up dart_sass

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,12 @@ config :pento, PentoWeb.Endpoint,
   secret_key_base: "/HAR4eesV2Bw4X21dpfqLAkAmkpuB6cQ1ZScgRK2xNYKAk1LBARag4ud0zYY2psw",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    sass: {
+      DartSass,
+      :install_and_run,
+      [:default, ~w(--embed-source-map --source-map-urls=absolute --watch)]
+    }
   ]
 
 # ## SSL Support
