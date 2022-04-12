3d8229a890204cbf56415747315c4c7eed7fae9f
kvxium <keith.ho@pixiumdigital.com>
2022-04-12 13:20:51 +0800

    :wrench: reset db

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -3,57 +3,61 @@ defmodule PentoWeb.WrongLive do
   import PentoWeb.Helpers.IconHelper
 
   def mount(_params, _session, socket) do
-    {:ok, assign(
-      socket, 
-      score: 0, 
-      message: "Make a guess:", 
-      current_time: time(), 
-      answer: :rand.uniform(10) |> to_string(),
-      correct_answer: false)}
+    {:ok,
+     assign(
+       socket,
+       score: 0,
+       message: "Make a guess:",
+       current_time: time(),
+       answer: :rand.uniform(10) |> to_string(),
+       correct_answer: false
+     )}
   end
-  
+
   # Order matters. This should precede the wrong option
-  def handle_event("guess", %{"number" => guess}=data, %{assigns: %{answer: guess}} = socket) do
+  def handle_event("guess", %{"number" => guess} = data, %{assigns: %{answer: guess}} = socket) do
     message = "Congratulations. Your guess #{guess} is correct. "
     score = socket.assigns.score + 1
+
     {
-    :noreply,
-    assign(
-    socket,
-    current_time: time(),
-    message: message,
-    score: score,
-    correct_answer: true
-    )
-  }
+      :noreply,
+      assign(
+        socket,
+        current_time: time(),
+        message: message,
+        score: score,
+        correct_answer: true
+      )
+    }
   end
 
   # NOTE: Pattern matching condition
-  def handle_event("guess", %{"number" => guess}=data, %{assigns: %{answer: answer}} = socket) do
-    IO.inspect(data);
-    IO.inspect(socket);
+  def handle_event("guess", %{"number" => guess} = data, %{assigns: %{answer: answer}} = socket) do
+    IO.inspect(data)
+    IO.inspect(socket)
     message = "Your guess: #{guess}. Wrong. Guess again. "
     score = socket.assigns.score - 1
+
     {
-    :noreply,
-    assign(
-    socket,
-    current_time: time(),
-    message: message,
-    score: score,
-    )
-  }
+      :noreply,
+      assign(
+        socket,
+        current_time: time(),
+        message: message,
+        score: score
+      )
+    }
   end
 
   def time() do
-    DateTime.utc_now |> to_string
+    DateTime.utc_now() |> to_string
   end
 
   def render(assigns) do
     ~H"""
     <style>
       .pento-score {
-        color: red;
+        # color: red;
       }
       .guess-box {
         width: 140px;
@@ -79,7 +83,7 @@ defmodule PentoWeb.WrongLive do
     <h2>Answer: <%= @answer %></h2>
     <h2><%= @message %></h2>
 
-    # Render SVG
+    <!-- Render SVG -->
     <%= PentoWeb.Helpers.IconHelper.icon_tag(@socket, "active", class: "something") %>
     <%= icon_tag(@socket, "active", class: "something") %>
     <h2>It's <%= @current_time %></h2>
@@ -95,5 +99,5 @@ defmodule PentoWeb.WrongLive do
     </h2>
     <% end %>
     """
-    end
+  end
 end
