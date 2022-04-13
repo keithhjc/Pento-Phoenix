fe4c315ea4580b97dab3a987dec1c5df38e7bbe6
kvxium <keith.ho@pixiumdigital.com>
2022-04-13 11:30:27 +0800

    :rocket: init product / catalog

--- lib/pento_web/router.ex
+++ lib/pento_web/router.ex
@@ -81,6 +81,12 @@ defmodule PentoWeb.Router do
     put "/users/settings", UserSettingsController, :update
     get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
 
+    live "/products", ProductLive.Index, :index
+    live "/products/new", ProductLive.Index, :new
+    live "/products/:id/edit", ProductLive.Index, :edit
+    live "/products/:id", ProductLive.Show, :show
+    live "/products/:id/show/edit", ProductLive.Show, :edit
+
     live_session :default, on_mount: PentoWeb.UserAuthLive do
       live "/guess", WrongLive
     end
