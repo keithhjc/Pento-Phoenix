81e22460f8fd3bc5c8a2551e0d669c57479352bb
kvxium <keith.ho@pixiumdigital.com>
2022-04-18 16:05:15 +0800

    :zap: image upload

--- lib/pento/catalog/product.ex
+++ lib/pento/catalog/product.ex
@@ -7,6 +7,7 @@ defmodule Pento.Catalog.Product do
     field :name, :string
     field :sku, :integer
     field :unit_price, :float
+    field :image_upload, :string
 
     timestamps()
   end
@@ -14,10 +15,11 @@ defmodule Pento.Catalog.Product do
   @doc false
   def changeset(product, attrs) do
     product
-    |> cast(attrs, [:name, :description, :unit_price, :sku])
+    |> cast(attrs, [:name, :description, :unit_price, :sku, :image_upload])
     |> validate_required([:name, :description, :unit_price, :sku])
     |> unique_constraint(:sku)
     |> validate_number(:unit_price, greater_than: 0.0)
+
     # |> validate_number(:unit_price, less_than: product.unit_price)
   end
 
--- lib/pento_web/live/product_live/form_component.ex
+++ lib/pento_web/live/product_live/form_component.ex
@@ -6,11 +6,19 @@ defmodule PentoWeb.ProductLive.FormComponent do
   @impl true
   def update(%{product: product} = assigns, socket) do
     changeset = Catalog.change_product(product)
+    Process.sleep(250)
 
     {:ok,
      socket
      |> assign(assigns)
-     |> assign(:changeset, changeset)}
+     |> assign(:changeset, changeset)
+     |> allow_upload(:image,
+      accept: ~w(.jpg .jpeg .png),
+      max_entries: 1,
+      max_file_size: 9_000_000,
+      auto_upload: true,
+      progress: &handle_progress/3
+      )}
   end
 
   @impl true
@@ -27,8 +35,48 @@ defmodule PentoWeb.ProductLive.FormComponent do
     save_product(socket, socket.assigns.action, product_params)
   end
 
-  defp save_product(socket, :edit, product_params) do
-    case Catalog.update_product(socket.assigns.product, product_params) do
+  defp handle_progress(:image, entry, socket) do
+    :timer.sleep(500)
+
+    IO.inspect("ENTRY")
+    IO.inspect(entry)
+
+    if entry.done? do
+      path =
+        consume_uploaded_entry(
+          socket,
+          entry,
+          &upload_static_file(&1, socket)
+        )
+
+      IO.inspect("[form_component] path: #{path}")
+
+      {:noreply, socket
+       |> put_flash(:info, "file #{entry.client_name} uploaded")
+       |> assign(:image_upload, path)
+      }
+    else
+      {:noreply, socket}
+    end
+  end
+
+  defp upload_static_file(%{path: path}, socket) do
+    # Plug in your production image file persistence implementation here!
+    dest = Path.join("priv/static/images", Path.basename(path))
+    File.cp!(path, dest)
+    {:ok, Routes.static_path(socket, "/images/#{Path.basename(dest)}")}
+  end
+
+  defp get_dest_filename(filename, id, type \\ "art") do
+    file_extension = Path.basename(filename) |> Path.extname() |> String.downcase()
+    # DBG: hms is a helper function giving a hhmmss.mmm time
+    # "#{type}_#{id}_#{hms()}#{file_extension}"
+  end
+
+  defp save_product(socket, :edit, params) do
+    result = Catalog.update_product(socket.assigns.product, product_params(socket, params))
+
+    case result do
       {:ok, _product} ->
         {:noreply,
          socket
@@ -41,8 +89,8 @@ defmodule PentoWeb.ProductLive.FormComponent do
     end
   end
 
-  defp save_product(socket, :new, product_params) do
-    case Catalog.create_product(product_params) do
+  defp save_product(socket, :new, params) do
+    case Catalog.create_product(product_params(socket, params)) do
       {:ok, _product} ->
         {:noreply,
          socket
@@ -53,4 +101,17 @@ defmodule PentoWeb.ProductLive.FormComponent do
         {:noreply, assign(socket, changeset: changeset)}
     end
   end
+
+  @impl Phoenix.LiveView
+  def handle_event("cancel-upload", %{"ref" => ref}, socket) do
+    {:noreply, cancel_upload(socket, :image, ref)}
+  end
+
+  def product_params(socket, params) do
+    Map.put(params, "image_upload", socket.assigns.image_upload)
+  end
+
+  def friendly_error(:too_large), do: "Image too large"
+  def friendly_error(:too_many_files), do: "Too many files"
+  def friendly_error(:not_accepted), do: "Unacceptable file type"
 end
